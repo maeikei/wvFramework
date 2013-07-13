6e3dbedb84ad1e75b307a26efce5ccfc344e34e6
ma yingkui <maeikei@gmail.com>
2013-07-13 22:42:58 +0900

    change to cpplibnetwork 0.10.2

--- client/wvmake/main.cpp
+++ client/wvmake/main.cpp
@@ -1,64 +1,82 @@
-//            Copyright (c) Glyn Matthews 2010.
-// Copyright 2012 Google, Inc.
+
+//          Copyright Dean Michael Berris 2008.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-//[ hello_world_client_main
+//[ http_client_main
 /*`
- This is a part of the 'Hello World' example.  We create a client
- object and make a single HTTP request.  If we use make this request
- to the `hello_world_server`, then the output is simply "Hello,
- World!".
+ This application takes a URL as a command line argument and prints
+ the resource to the console.
  */
-#include <network/protocol/http/client.hpp>
+#include <boost/program_options.hpp>
+#include <boost/network/protocol/http.hpp>
+#include <string>
+#include <utility>
 #include <iostream>
 
-namespace http = network::http;
+namespace po = boost::program_options;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char * argv[]) {
+    using namespace boost::network;
+    po::options_description options("Allowed options");
+    std::string output_filename, source;
+    bool show_headers;
+    options.add_options()
+    ("help,h", "produce help message")
+    ("headers,H", "print headers")
+    ("source,s", po::value<std::string>(&source), "source URL")
+    ;
     
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " url" << std::endl;
-        return 1;
-    }
-    
+    po::positional_options_description positional_options;
+    positional_options.add("source", 1);
+    po::variables_map vm;
     try {
-        /*<< Creates the client. >>*/
-        http::client client;
-        /*<< Creates a request using a URI supplied on the command
-         line. >>*/
-        http::client::request request(argv[1]);
-#if 0
-        request.set_method("GET");
-        std::string reqqbody;
-        request.get_body(reqqbody);
-        std::cout << "reqqbody=<"<< reqqbody  << ">" << std::endl;
-
-        request.get_headers( [](std::string const&name, std::string const&value)
-                            {
-                                std::cout << "name=<"<< name <<">value=<"<< value << ">" << std::endl;
-                            }
-                            );
-#endif
-        /*<< Gets a response from the HTTP server. >>*/
-        http::client::response response = client.get(request);
-        /*<< Prints the response headers to the console. >>*/
-#if 0
-        auto h = headers(response);
-        std::multimap<std::string, std::string> hmap = h;
-        for (auto const & pair :hmap ) {
-            std::cout << "pair.first=<"<< pair.first <<">pair.second=<"<< pair.second << ">" << std::endl;
-        }
-#endif
-        /*<< Prints the response body to the console. >>*/
-        std::cout << body(response) << std::endl;
-    }
-    catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
+        po::store(po::command_line_parser(argc, argv).options(options).positional(positional_options).run(),
+                  vm);
+        po::notify(vm);
+    } catch(std::exception & e) {
+        std::cout << "Error: " << e.what() << std::endl;
+        std::cout << options << std::endl;
+        return EXIT_FAILURE;
+    };
     
-    return 0;
+    if (vm.count("help")) {
+        std::cout << options << std::endl;
+        return EXIT_SUCCESS;
+    };
+    
+    if (vm.count("source") < 1) {
+        std::cout << "Error: Source URL required." << std::endl;
+        std::cout << options << std::endl;
+        return EXIT_FAILURE;
+    };
+    
+    show_headers = vm.count("headers") ? true : false ;
+    
+    
+    typedef http::basic_client<http::tags::http_async_8bit_tcp_resolve, 1, 0>
+    http_client;
+    
+    http_client::request request(source);
+    http_client::string_type destination_ = host(request);
+    
+    request << ::boost::network::header("Connection", "close");
+    http_client::options client_options;
+    http_client client(client_options.follow_redirects(true));
+    http_client::response response = client.get(request);
+    
+    if (show_headers) {
+        headers_range<http_client::response>::type headers_ = response.headers();
+        typedef std::pair<std::string, std::string> header_type;
+        BOOST_FOREACH(header_type const & header, headers_) {
+            std::cout << header.first << ": " << header.second << std::endl;
+        }
+        std::cout << std::endl;
+    };
+    
+    body_range<http_client::response>::type body_ = body(response).range();
+    boost::copy(body_, std::ostream_iterator<char_<http_client::request::tag>::type>(std::cout));
+    return EXIT_SUCCESS;
 }
 //]
