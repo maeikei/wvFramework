2b13d2528f6fde293f04672ea085474c33d474d7
ma yingkui <maeikei@gmail.com>
2015-01-17 23:58:56 +0900

    Update routes.cpp

--- approot/app/Http/routes.cpp
+++ approot/app/Http/routes.cpp
@@ -12,7 +12,7 @@ struct router
 {
   router(void) 
   {
-    Route::get('/', 'WelcomeController@index');
+    Route::get('/', 'HomeController@index');
     Route::get('home', 'HomeController@index');
   }
 };
