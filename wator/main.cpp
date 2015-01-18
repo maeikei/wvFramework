e73ab501e87bac11f9b18d5348ba943c8969dfdb
ma yingkui <maeikei@gmail.com>
2015-01-18 22:36:29 +0900

    Update main.cpp

--- wator/main.cpp
+++ wator/main.cpp
@@ -5,10 +5,10 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
-extern http_main(void);
+extern int http_main(const string &ip,const string &port,const string &root);
 
 int main(int argc, char* argv[])
 {
-  http_main();
+  http_main("127.0.0.1","8080","./");
   return 0;
 }
