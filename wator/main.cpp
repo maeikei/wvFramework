9ffb0fe9470ebb28ada6319401ef488301ee2c54
ma yingkui <maeikei@gmail.com>
2015-01-18 22:32:18 +0900

    Update main.cpp

--- wator/main.cpp
+++ wator/main.cpp
@@ -1,2 +1,14 @@
-/*
-*/
+//
+// main.cpp
+//
+
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+extern http_main(void);
+
+int main(int argc, char* argv[])
+{
+  http_main();
+  return 0;
+}
