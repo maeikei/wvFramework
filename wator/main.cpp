b566e36a5b5b7bf1915c41f4c954e00eee83f89d
ma yingkui <maeikei@gmail.com>
2015-01-18 22:39:41 +0900

    Update main.cpp

--- wator/main.cpp
+++ wator/main.cpp
@@ -10,6 +10,6 @@ extern int http_main(const string &ip,const string &port,const string &root);
 
 int main(int argc, char* argv[])
 {
-  http_main("127.0.0.1","8080","./");
+  http_main("127.0.0.1","4447","./");
   return 0;
 }
