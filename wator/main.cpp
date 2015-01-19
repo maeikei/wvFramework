ac6596d79f3e2d2730ebe6c28b0dbf57f08f0f02
ma yingkui <maeikei@gmail.com>
2015-01-19 23:51:40 +0900

    Update main.cpp

--- wator/main.cpp
+++ wator/main.cpp
@@ -4,14 +4,22 @@
 
 #include <iostream>
 #include <string>
-#include <boost/filesystem.hpp>
 using namespace std;
+#include <boost/filesystem.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+using namespace boost::log;
+#include "debug.h"
+
 extern int http_main(const string &ip,const string &port,const string &root);
 
 int main(int argc, char* argv[])
 {
+  core::get()->set_filter(trivial::severity >= trivial::info);
+  
   boost::filesystem::path full_path( boost::filesystem::current_path() );
-  std::cout << "Current path is : " << full_path << std::endl;
+  BOOST_LOG_TRIVIAL(info) << "Current path is : " << full_path;
   http_main("0.0.0.0","4447",full_path.string());
   return 0;
 }
