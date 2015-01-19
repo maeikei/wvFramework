d8b2a84fa52fd493ccf6c72e370f321d1f8f429e
ma yingkui <maeikei@gmail.com>
2015-01-19 23:28:30 +0900

    Update main.cpp

--- wator/main.cpp
+++ wator/main.cpp
@@ -4,12 +4,14 @@
 
 #include <iostream>
 #include <string>
-#include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
 using namespace std;
 extern int http_main(const string &ip,const string &port,const string &root);
 
 int main(int argc, char* argv[])
 {
+  boost::filesystem::path full_path( boost::filesystem::current_path() );
+  std::cout << "Current path is : " << full_path << std::endl;
   http_main("0.0.0.0","4447","./");
   return 0;
 }
