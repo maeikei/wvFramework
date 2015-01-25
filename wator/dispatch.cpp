b912f95e7458901018d79c5e841a2b31a0e23f98
ma yingkui <maeikei@gmail.com>
2015-01-25 22:53:08 +0900

    Update dispatch.cpp

--- wator/dispatch.cpp
+++ wator/dispatch.cpp
@@ -9,10 +9,18 @@ Dispatcher::Dispatcher(const http::server::request &req,http::server::reply& rep
 :req_(req)
 ,rep_(rep)
 {
-  
+  LOG_DEBUG(req_.method);
+  LOG_DEBUG(req_.uri);
+  LOG_DEBUG(req_.http_version_major);
+  LOG_DEBUG(req_.http_version_minor);
+  for(auto head :req_.headers)
+  {
+    LOG_DEBUG(head.name);
+    LOG_DEBUG(head.value);
+  }
 }
 bool Dispatcher::run(void)
 {
-  
+  return true;
 }
  
