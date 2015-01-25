f8156f095080ed2e352016aee659ae6f19a1b94b
ma yingkui <maeikei@gmail.com>
2015-01-25 22:28:50 +0900

    Update and rename router.hpp to dispatch.hpp

---
+++ wator/dispatch.hpp
@@ -0,0 +1,13 @@
+#ifndef __DISPATCH_HPP__
+#define __DISPATCH_HPP__
+namespace WatorVapor
+{
+  class Dispatcher
+  {
+    public:
+      Dispatcher();
+    private:
+      
+  };
+} // namespace WatorVapor
+#endif// __DISPATCH_HPP__
