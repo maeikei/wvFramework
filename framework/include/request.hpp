00e81bd04299e3e93f1db649994147790195e036
ma yingkui <maeikei@gmail.com>
2015-01-25 23:10:50 +0900

    Create request.hpp

---
+++ framework/include/request.hpp
@@ -0,0 +1,27 @@
+//
+// request.hpp
+// ~~~~~~~~~~~
+
+#ifndef __WATOR_REQUEST_HPP__
+#define __WATOR_REQUEST_HPP__
+
+#include <string>
+#include <map>
+
+namespace wator {
+namespace http {
+
+/// A request received from a client.
+struct request
+{
+  std::string method;
+  std::string uri;
+  int http_version_major;
+  int http_version_minor;
+  std::map<string,string> headers;
+};
+
+} // namespace http
+} // namespace wator
+
+#endif // __WATOR_REQUEST_HPP__
