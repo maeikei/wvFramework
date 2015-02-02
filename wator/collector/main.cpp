e39fae2942015e8ba64e5d6cf44c1e9011d17cd5
ma yingkui <maeikei@gmail.com>
2015-02-02 23:47:21 +0900

    Update main.cpp

--- wator/collector/main.cpp
+++ wator/collector/main.cpp
@@ -1,7 +1,12 @@
 #include <string>
 using namespace std;
 #include "debug.h"
+#include <boost/filesystem.hpp>
+
 void collector_main(const string &root)
 {
   LOG_INFO(root);
+  boost::filesystem::controller(root);
+  LOG_INFO(controller);
+  
 }
