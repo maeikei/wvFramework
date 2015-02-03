941084591f3cacaa73e200338f9da441bdc46ded
ma yingkui <maeikei@gmail.com>
2015-02-04 00:07:10 +0900

    Update ACController.hpp

--- wator/collector/ACController.hpp
+++ wator/collector/ACController.hpp
@@ -1,11 +1,15 @@
 #ifndef __AUTO_COMPELETE_CONTROLLER_HPP__
 #define __AUTO_COMPELETE_CONTROLLER_HPP__
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#include <string>
+
 namespace wator {
 namespace ac {
   class ACController
   {
     public:
-      ACController();
+      ACController(const fs::path &p,const std::string &root);
     private:
       ACController();
   }
