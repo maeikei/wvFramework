c9f6dc299978d85ca2abeaba087a0a9f04fb59bc
ma yingkui <maeikei@gmail.com>
2015-02-04 00:15:27 +0900

    Update ACBuilder.hpp

--- wator/collector/ACBuilder.hpp
+++ wator/collector/ACBuilder.hpp
@@ -1,1 +1,19 @@
+#ifndef __AUTO_COMPELETE_BUILDER_HPP__
+#define __AUTO_COMPELETE_BUILDER_HPP__
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#include <string>
 
+namespace wator {
+namespace ac {
+  class ACBuilder
+  {
+    public:
+      ACBuilder(const fs::path &p,const std::string &root);
+    private:
+      ACBuilder();
+  };
+} // ac
+} // wator
+
+#endif //__AUTO_COMPELETE_BUILDER_HPP__
