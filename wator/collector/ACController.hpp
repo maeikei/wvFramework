6ce9d667f5c5dde86cebfaf25ce30d132073f076
ma yingkui <maeikei@gmail.com>
2015-02-04 00:04:21 +0900

    Create ACController.hpp

---
+++ wator/collector/ACController.hpp
@@ -0,0 +1,12 @@
+#ifndef __AUTO_COMPELETE_CONTROLLER_HPP__
+#define __AUTO_COMPELETE_CONTROLLER_HPP__
+namespace wator {
+namespace ac {
+  class ACController
+  {
+    
+  }
+} // ac
+} // wator
+
+#endif //__AUTO_COMPELETE_CONTROLLER_HPP__
