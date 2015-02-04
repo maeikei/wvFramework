072e0a84a06b4260a1a71253914a7f7e8b0a8ea5
ma yingkui <maeikei@gmail.com>
2015-02-05 00:04:01 +0900

    Update ACBuilder.cpp

--- wator/collector/ACBuilder.cpp
+++ wator/collector/ACBuilder.cpp
@@ -1,10 +1,32 @@
 #include "ACBuilder.hpp"
 using namespace wator::ac;
+#include "debug.h"
+
+/*
+project({name})
+cmake_minimum_required(VERSION 2.8)
+include_directories({root})
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11 -pthread")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -pthread")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -std=c++11 -pthread")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+*/
+
 ACBuilder::ACBuilder(const std::string &root)
+:output_()
 {
-  
+  output_ += "project(";
+  output_ += ")\n";
+  output_ += "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -std=c11 -pthread\")\n";
+  output_ += "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11 -pthread")\n";
+  output_ += "set(CMAKE_EXE_LINKER_FLAGS \"${CMAKE_EXE_LINKER_FLAGS} -std=c++11 -pthread\")\n";
+  output_ += "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -Wall\")\n";
+  output_ += "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -Wall\")\n";
+
 }
 bool ACBuilder::gen(void)
 {
+  LOG_INFO(output_);
   return true;
 }
