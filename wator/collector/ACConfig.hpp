a5914fed42dbdca3c208e914027eb7ec551bd8b4
ma yingkui <maeikei@gmail.com>
2015-02-04 23:38:37 +0900

    Update ACConfig.hpp

--- wator/collector/ACConfig.hpp
+++ wator/collector/ACConfig.hpp
@@ -1,1 +1,20 @@
+#ifndef __AUTO_COMPELETE_CONFIG_HPP__
+#define __AUTO_COMPELETE_CONFIG_HPP__
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#include <string>
+
+namespace wator {
+namespace ac {
+  class ACConfig
+  {
+    public:
+      ACConfig(const std::string &root);
+    private:
+      ACConfig();
+  };
+} // ac
+} // wator
+
+#endif //__AUTO_COMPELETE_CONFIG_HPP__
 
