4b1662199b50f741914b8b0317b61461d2ba2498
ma yingkui <maeikei@gmail.com>
2015-02-05 00:09:03 +0900

    Update ACBuilder.cpp

--- wator/collector/ACBuilder.cpp
+++ wator/collector/ACBuilder.cpp
@@ -21,7 +21,7 @@ ACBuilder::ACBuilder(const std::string &root)
   output_ += "cmake_minimum_required(VERSION 2.8)\n";
   output_ += "include_directories(";
   output_ += root;
-  output_ += ")";
+  output_ += ")\n";
   output_ += "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -std=c11 -pthread\")\n";
   output_ += "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11 -pthread\")\n";
   output_ += "set(CMAKE_EXE_LINKER_FLAGS \"${CMAKE_EXE_LINKER_FLAGS} -std=c++11 -pthread\")\n";
