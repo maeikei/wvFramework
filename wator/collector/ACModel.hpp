c12e92f688eed8c7dcd6918eaad4283676f687dd
ma yingkui <maeikei@gmail.com>
2015-02-04 23:40:30 +0900

    Update ACModel.hpp

--- wator/collector/ACModel.hpp
+++ wator/collector/ACModel.hpp
@@ -1,1 +1,19 @@
+#ifndef __AUTO_COMPELETE_MODEL_HPP__
+#define __AUTO_COMPELETE_MODEL_HPP__
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#include <string>
 
+namespace wator {
+namespace ac {
+  class ACModel
+  {
+    public:
+      ACModel(const std::string &root);
+    private:
+      ACModel();
+  };
+} // ac
+} // wator
+
+#endif //__AUTO_COMPELETE_MODEL_HPP__
