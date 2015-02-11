a1f3e54bff8de4c22fb44dcbac205aaa0375c317
ma yingkui <maeikei@gmail.com>
2015-02-11 23:46:21 +0900

    Update ACBuilder.cpp

--- wator/collector/ACBuilder.cpp
+++ wator/collector/ACBuilder.cpp
@@ -16,24 +16,24 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 */
 
 ACBuilder::ACBuilder(const fs::path &root)
-:output_()
+:cmake_()
 {
-  output_ += "project(";
-  output_ += root.leaf().string();
-  output_ += ")\n";
-  output_ += "cmake_minimum_required(VERSION 2.8)\n";
-  output_ += "include_directories(";
-  output_ += root.string();
-  output_ += ")\n";
-  output_ += "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -std=c11 -pthread\")\n";
-  output_ += "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11 -pthread\")\n";
-  output_ += "set(CMAKE_EXE_LINKER_FLAGS \"${CMAKE_EXE_LINKER_FLAGS} -std=c++11 -pthread\")\n";
-  output_ += "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -Wall\")\n";
-  output_ += "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -Wall\")\n";
+  cmake_ += "project(";
+  cmake_ += root.leaf().string();
+  cmake_ += ")\n";
+  cmake_ += "cmake_minimum_required(VERSION 2.8)\n";
+  cmake_ += "include_directories(";
+  cmake_ += root.string();
+  cmake_ += ")\n";
+  cmake_ += "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -std=c11 -pthread\")\n";
+  cmake_ += "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11 -pthread\")\n";
+  cmake_ += "set(CMAKE_EXE_LINKER_FLAGS \"${CMAKE_EXE_LINKER_FLAGS} -std=c++11 -pthread\")\n";
+  cmake_ += "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -Wall\")\n";
+  cmake_ += "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -Wall\")\n";
 
 }
 bool ACBuilder::gen(void)
 {
-  LOG_INFO(output_);
+  LOG_INFO(cmake_);
   return true;
 }
