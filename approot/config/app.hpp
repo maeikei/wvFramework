24f57138b9029f1acdcb9392fa7482c70462be93
ma yingkui <maeikei@gmail.com>
2015-03-03 00:23:29 +0900

    Update app.hpp

--- approot/config/app.hpp
+++ approot/config/app.hpp
@@ -93,14 +93,3 @@ class app
 
 #endif
 
-struct app
-{
-	static const bool debug;
-	static const string url;
-	static const string timezone;
-	static const string locale;
-	static const string fallback_locale;
-	static const string key;
-	static const string cipher;
-	static const string log;	
-};
