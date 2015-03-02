de29cd4620333edaef455426764cf7f56251ee9e
ma yingkui <maeikei@gmail.com>
2015-03-03 00:24:03 +0900

    Update and rename framework/include to framework/include/app.hpp

---
+++ framework/include/app.hpp
@@ -0,0 +1,11 @@
+struct app
+{
+	static const bool debug;
+	static const string url;
+	static const string timezone;
+	static const string locale;
+	static const string fallback_locale;
+	static const string key;
+	static const string cipher;
+	static const string log;	
+};
